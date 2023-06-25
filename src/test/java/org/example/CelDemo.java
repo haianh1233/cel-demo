95855ebaa75f699971d7ccef301413dd806239d1
anhnguyen <nguyendanghaianh1233@gmail.com>
2023-06-25 15:41:34 +0700

    add sample handle raw json

--- src/test/java/org/example/CelDemo.java
+++ src/test/java/org/example/CelDemo.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.projectnessie.cel.checker.Decls;
 import org.projectnessie.cel.tools.Script;
+import org.projectnessie.cel.tools.ScriptCreateException;
 import org.projectnessie.cel.tools.ScriptException;
 import org.projectnessie.cel.tools.ScriptHost;
 import org.projectnessie.cel.types.jackson.JacksonRegistry;
@@ -95,6 +99,69 @@ public class CelDemo {
         Map<String, Object> arguments = new HashMap<>();
         arguments.put(inputKey, input);
         arguments.put(checkNameKey, checkNameValue);
+
+        boolean result = script.execute(Boolean.class, arguments);
+
+        System.out.println("Result: " + result);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void buildCheckInputSizeValidationScriptForJson() throws ScriptException {
+        String inputKey = "input";
+        String inputNameValue = "David";
+
+        Input input = Input.builder()
+                .name(inputNameValue)
+                .build();
+
+        ScriptHost scriptHost = ScriptHost.newBuilder()
+                .registry(JacksonRegistry.newRegistry())
+                .build();
+
+        Script script = scriptHost.buildScript("size(input.name) == " + inputNameValue.length())
+                .withDeclarations(
+                        Decls.newVar(inputKey, Decls.newObjectType(Input.class.getName()))
+                )
+                .withTypes(Input.class)
+                .build();
+
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put(inputKey, input);
+
+        boolean result = script.execute(Boolean.class, arguments);
+
+        System.out.println("Result: " + result);
+        Assertions.assertTrue(result);
+
+    }
+
+    @Test
+    public void buildCheckInputSizeValidationScriptForRawJson() throws ScriptException, JsonProcessingException {
+        String inputKey = "input";
+        String inputJson = "{\"name\":\"hai\",\"address\":{\"city\":\"hochiminh\",\"street\":\"nguyenhuucanh\"}}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<Map<String, Object>> typeReference = new TypeReference<>() {};
+
+        Map<String, Object> input = objectMapper.readValue(inputJson, typeReference);
+
+        ScriptHost scriptHost = ScriptHost.newBuilder()
+                .registry(JacksonRegistry.newRegistry())
+                .build();
+
+        Script script = scriptHost.buildScript(
+                "input.name == 'hai' " +
+                        "&& input.address.city == 'hochiminh' " +
+                        "&& size(input.address.street) > 5"
+                )
+                .withDeclarations(
+                        Decls.newVar(inputKey, Decls.Any)
+                )
+                .build();
+
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put(inputKey, input);
 
         boolean result = script.execute(Boolean.class, arguments);
 
